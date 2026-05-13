6476dcafe2a4f660a008cc0073a2402680db75d8
bagusim20 <e31252385@student.polije.ac.id>
2026-05-13 13:26:52 +0700

    Delete PROJEK SIPAUDQU directory

--- PROJEK SIPAUDQU/App/Layout/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer-bar">
-    <div class="footer-content">
-        2026 SIPAUDQU - Sistem Informasi Psud Qur'an
-    </div>
-</footer>
--- PROJEK SIPAUDQU/App/Layout/navbar.php
+++
@@ -1,5 +0,0 @@
-<div class="navbar">
-    <div class="title">
-        <h1>SIPAUDQU</h1>
-    </div>
-</div>
